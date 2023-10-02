5d50fe4159a97c257b490a076fd21747bafba623
Pablo Castro <pablocas-tro1@outlook.com>
2023-10-02 12:48:18 -0400

    Updated database

--- server.js
+++ server.js
@@ -15,3 +15,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
+
+// render url: https://spa-api-t1tj.onrender.com/spa_services
